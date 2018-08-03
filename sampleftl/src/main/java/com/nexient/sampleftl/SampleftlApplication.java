22c5b04754ad868f8d87810879523be2f9ae38b7
Van Hieu Tran <vtran@nexient.com>
2018-08-03 11:30:11 -0400

    example spring boot with freemarker

---
+++ sampleftl/src/main/java/com/nexient/sampleftl/SampleftlApplication.java
@@ -0,0 +1,12 @@
+package com.nexient.sampleftl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleftlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleftlApplication.class, args);
+	}
+}
